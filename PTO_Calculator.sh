67fc45a45b10a2214ed43694dd007cdb75cf3fc6
Meir Lazar <meirlazar@gmail.com>
2019-11-27 12:40:53 -0500

    Add files via upload

---
+++ PTO_Calculator.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# Written by Meir Lazar
+# Version 3.1 11/27/2019
+
+# Purpose: This script, using the PTO accrual rate and the amount of PTO you currently have saved up, will calculate how much PTO you will have at every pay period, both in hours and days.
+# It will also show which Jewish Observant Holidays will be taken off and calculate your PTO based on that data.
+
+# Further versions will have Federal Holidays automatically shown in the script, as well as stop dates.
+
+# 6.67 hours of accural per pay period (1-15, 16-30)
+# 24 pay periods x 6.67 = 160 hours of PTO 
+# 160 hrs / 8hrs per day = 20 days of PTO 
+# 20 days / 5 Workdays per week = 4 weeks of PTO per calendar year
+
+#######################################################################################################
+#######################################################################################################
+	
+# User defined Variables 
+	
+# Accural rates per pay period (15th and the last day of month)
+ACCRATE=6.67   # This is the amount of PTO (in hours) that you accrue per pay period - CHANGE IT
+
+#######################################################################################################
+#######################################################################################################
+
+
+# User Questions
+read -p "What is the amount of PTO you currently have?": CURRPTO
+IFS=: read -p "Type the starting date MM:DD:YYYY to check the relevant Jewish Holidays and your PTO balance": MONTH DAY YEAR 
+if [[ -z $MONTH ]]; then IFS=: read MONTH DAY YEAR <<< $(date +"%m:%d:%y"); fi
+
+#######################################################################################################
+
+# Takes a dump of all Jewish Holidays on the specified calendar year
+ALLJEWISHHOLIDAYS=$(lynx -dump "http://jewishholidaysonline.com/$YEAR" | grep -E "Monday,|Tuesday,|Wednesday,|Thursday,|Friday,|Saturday,|Sunday,")
+
+TMPDAY=${DAY}
+TMPMONTH=${MONTH}
+FULLMONTH=$(date -d $YEAR-$TMPMONTH-$TMPDAY '+%B')
+
+#######################################################################################################
+
+while [[ -z $JEWISHHOLIDAYS ]]; do 
+JEWISHHOLIDAYS=$(echo "${ALLJEWISHHOLIDAYS}" | grep -w -A 100 "$FULLMONTH ${TMPDAY},")
+TMPDAY=$(printf "%02d" $(( TMPDAY + 1 ))) 
+if [[ $TMPDAY > 31 ]]; then TMPDAY=01; TMPMONTH=$(printf "%02d" $(( TMPMONTH + 1 ))); FULLMONTH=$(date -d $YEAR-$TMPMONTH-$TMPDAY '+%B'); fi
+if [[ $TMPMONTH > 12 ]]; then echo "No Jewish Holidays left this Year"; break; fi
+done
+
+# List of dates in an array (in the Jewish calendar) that Observant Jews are not allowed to work so must take PTO. 
+
+JHOLIDAYS[1]="14th of Adar"  # Purim day
+JHOLIDAYS[2]="15th of Nisan" # 1st day of passover
+JHOLIDAYS[3]="16th of Nisan" # 2nd day of Passover
+JHOLIDAYS[4]="21st of Nisan" # 7th day of Passover 
+JHOLIDAYS[5]="22nd of Nisan" # 8th day of Passover
+JHOLIDAYS[6]="6th of Sivan" # 1st day of Shavuot
+JHOLIDAYS[7]="7th of Sivan" # 2nd day of Shavuot
+JHOLIDAYS[8]="1st of Tishrei" # 1st day of Rosh Hashanah
+JHOLIDAYS[9]="2nd of Tishrei" # 2nd day of Rosh Hashanah
+JHOLIDAYS[10]="10th of Tishrei" # Yom Kippur day
+JHOLIDAYS[11]="15th of Tishrei" # 1st day of Sukkot
+JHOLIDAYS[12]="16th of Tishrei" # 2nd day of Sukkot
+JHOLIDAYS[13]="22nd of Tishrei" # Shmini Atzeret
+JHOLIDAYS[14]="23rd of Tishrei" # Simchat Torah
+
+
+for item in "${JHOLIDAYS[@]}"; do 
+OFF=$(echo "${JEWISHHOLIDAYS}" | grep -w "$item" | head -1 | grep -E "Monday|Tuesday|Wednesday|Thursday|Friday")
+if [[ -n $OFF ]]; then count=$(( count + 1)); WORKDAYSOFF[$count]="${OFF}"; fi
+done
+
+echo "--------------------------------------------------"
+echo "${#JHOLIDAYS[*]} days of holidays occur this year"
+echo "${#WORKDAYSOFF[*]} Jewish holidays occur on weekdays that will be used from your PTO"
+echo "--------------------------------------------------"
+
+echo "Days to take PTO:"
+echo "--------------------------------------------------"
+for x in "${WORKDAYSOFF[@]}"; do echo "$x"; done
+echo "--------------------------------------------------"
+
+JHOLPTOHOURS=$(echo "${#WORKDAYSOFF[*]} * 8" | bc)
+echo "$JHOLPTOHOURS hours of PTO will be used for Jewish Holidays"
+
+#######################################################################################################
+
+# This part shows each pay date (till the end of the Gregorian year) with the amount of PTO accrued
+
+for (( m=$MONTH; m<13; m++ )); do 
+	if [[ "$m" == "12" ]]; then YEAR=$(( YEAR + 1 )); NEXTMO=01; else NEXTMO=$(( m + 1)); fi
+
+	# if day is before the 15th
+	if [[ $DAY > 15 ]]; then 
+		CURRPTO=$(echo "scale=2; $CURRPTO + $ACCRATE" | bc)
+		LASTDAY=$(date -d "$YEAR-$NEXTMO-01 -1 day" +%d)
+		echo "On $m/$LASTDAY/$YEAR - Total Accrued = $CURRPTO"
+		unset DAY
+		continue
+	fi
+
+	# 15th of the month
+	CURRPTO=$(echo "scale=2; $CURRPTO + $ACCRATE" | bc)
+	echo "On $m/15/$YEAR - Total Accrued = $CURRPTO"
+
+
+	# Last day of the month
+	CURRPTO=$(echo "scale=2; $CURRPTO + $ACCRATE" | bc)
+	LASTDAY=$(date -d "$YEAR-$NEXTMO-01 -1 day" +%d)
+	echo "On $m/$LASTDAY/$YEAR - Total Accrued = $CURRPTO"
+done
+
+#######################################################################################################
+#######################################################################################################
+
+# This section shows the total hours/days of PTO accrued through the end of the Gregorian year, then shows the difference once the Jewish Holidays have been substracted.
+
+# Print the total PTO hours/days
+PTOHOURS=$CURRPTO
+PTODAYS=$(echo "scale=2; $PTOHOURS / 8" | bc)
+echo "$PTOHOURS - Total Hours of PTO accrued by the end of the year"
+echo -e "$PTODAYS - Total days of PTO accrued by the end of the year\n"
+
+
+
+# Print the PTO hours/days after the Jewish Holidays have been subtracted
+PTOHOURSLEFT=$(echo "scale=2; $PTOHOURS - $JHOLPTOHOURS" | bc)
+PTODAYSLEFT=$(echo "scale=2; $PTOHOURSLEFT / 8" | bc)
+echo "$PTOHOURSLEFT - Balance of hours of PTO left over by the end of the year after Jewish Holidays have been subtracted"
+echo "$PTODAYSLEFT - Balance of days of PTO left over by the end of the year after Jewish Holidays have been substracted"
+
+
+
+
+
+
+
